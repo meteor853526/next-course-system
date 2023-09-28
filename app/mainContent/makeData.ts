b3af51595233eca5d35937e1337653f2a5e04ad4
@fcu-d0989427 <75241765+meteor853526@users.noreply.github.com>
2023-09-28 21:59:15 +0800

    update

---
+++ app/mainContent/makeData.ts
@@ -0,0 +1,34 @@
+export const data: Employee[] = [
+    {
+      firstName: 'Dusty',
+      lastName: 'Kuvalis',
+      email: 'Randy63@yahoo.com',
+      jobTitle: 'Chief Creative Technician',
+      salary: 52729,
+      startDate: '3/20/2014',
+      signatureCatchPhrase: 'Cross-platform disintermediate workforce',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg',
+    },
+    {
+      firstName: "D'angelo",
+      lastName: 'Moen',
+      email: 'Andrew88@hotmail.com',
+      jobTitle: 'Forward Response Engineer',
+      salary: 71964,
+      startDate: '3/9/2018',
+      signatureCatchPhrase: 'Virtual local support',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg',
+    },
+];
+type Employee = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    jobTitle: string;
+    salary: number;
+    startDate: string;
+    signatureCatchPhrase: string;
+    avatar: string;
+  };
