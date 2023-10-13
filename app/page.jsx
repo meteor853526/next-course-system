c79a8cd6bbda075cfaa3a917c66323ac4a259b73
@fcu-d0989427 <75241765+meteor853526@users.noreply.github.com>
2023-10-13 16:13:10 +0800

    update

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main >
-      <h1>dashboard</h1>
+      <h1 className='bg-slate-200'>dashboard</h1>
     </main>
   )
 }
---
+++ components/makeData.ts
@@ -0,0 +1,111 @@
+export const data: Employee[] = [
+    {
+      firstName: 'Dusty',
+      lastName: 'Kuvalis',
+      email: 'Randy63@yahoo.com',
+      jobTitle: 'Chief ',
+      salary: 52729,
+      startDate: '3/20/2014',
+      signatureCatchPhrase: 'Cross-platform ',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/106.jpg',
+    },
+    {
+      firstName: "D'angelo",
+      lastName: 'Moen',
+      email: 'Andrew88@hotmail.com',
+      jobTitle: 'Forward',
+      salary: 71964,
+      startDate: '3/9/2018',
+      signatureCatchPhrase: 'Virtual local support',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/420.jpg',
+    },
+    {
+      firstName: 'Jameson',
+      lastName: 'Mayer',
+      email: 'Rosamond_Schuster@yahoo.com',
+      jobTitle: 'Regional ',
+      salary: 80916,
+      startDate: '10/30/2017',
+      signatureCatchPhrase: 'Front-line ',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1205.jpg',
+    },
+    {
+      firstName: 'Madaline',
+      lastName: 'Quitzon',
+      email: 'Alex_Grimes82@hotmail.com',
+      jobTitle: 'Corporate ',
+      salary: 68052,
+      startDate: '1/17/2018',
+      signatureCatchPhrase: 'Right-sized ',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/432.jpg',
+    },
+    {
+      firstName: 'Wilfrid',
+      lastName: 'Vandervort',
+      email: 'Buddy.Torphy@gmail.com',
+      jobTitle: 'Legacy  ',
+      salary: 85573,
+      startDate: '8/4/2014',
+      signatureCatchPhrase: 'Focused   ',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1131.jpg',
+    },
+    {
+      firstName: 'Chelsie',
+      lastName: 'Mraz',
+      email: 'Ladarius_Thiel70@yahoo.com',
+      jobTitle: 'Forward  ',
+      salary: 51062,
+      startDate: '1/6/2021',
+      signatureCatchPhrase: 'Diverse attitude-oriented migration',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1099.jpg',
+    },
+    {
+      firstName: 'Hassie',
+      lastName: 'Bruen',
+      email: 'Clair76@gmail.com',
+      jobTitle: 'Human  ',
+      salary: 61196,
+      startDate: '4/28/2016',
+      signatureCatchPhrase: 'Upgradable composite methodology',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/401.jpg',
+    },
+    {
+      firstName: 'Daisy',
+      lastName: 'Hane',
+      email: 'Alverta7@hotmail.com',
+      jobTitle: 'National  ',
+      salary: 25394,
+      startDate: '3/2/2020',
+      signatureCatchPhrase: 'Exclusive next generation initiative',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/436.jpg',
+    },
+    {
+      firstName: 'Wilbert',
+      lastName: 'Monahan',
+      email: 'Cydney.Jakubowski9@yahoo.com',
+      jobTitle: 'Internal  ',
+      salary: 79355,
+      startDate: '4/5/2017',
+      signatureCatchPhrase: 'Total asynchronous strategy',
+      avatar:
+        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/74.jpg',
+    },
+];
+type Employee = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    jobTitle: string;
+    salary: number;
+    startDate: string;
+    signatureCatchPhrase: string;
+    avatar: string;
+  };
